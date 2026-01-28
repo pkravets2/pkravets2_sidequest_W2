9aa0821e391ea081b13afd5542d6be7a29f73e2a
kravp5979 <pkravets@uwaterloo.ca>
2026-01-27 21:47:34 -0500

    higher values

--- sketch.js
+++ sketch.js
@@ -9,12 +9,12 @@ let blob = {
   points: 48, // Number of vertices around the circle (higher = smoother)
 
   // Shape deformation settings
-  wobble: 18, // Maximum amount the edge can move in or out
-  wobbleFreq: 2.5, // Controls how lumpy or smooth the blob looks
+  wobble: 78, // Maximum amount the edge can move in or out
+  wobbleFreq: 0.5, // Controls how lumpy or smooth the blob looks
 
   // Time values for animation
   t: 0, // Time input for noise()
-  tSpeed: 0.09, // How fast the blob "breathes"
+  tSpeed: 0.2, // How fast the blob "breathes"
 };
 
 function setup() {
